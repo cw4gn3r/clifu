ab728b4b119c4f8cf6edef4d96eb80c3b300ced4
Christoph Wagner <cwagner@oversee.net>
2013-02-09 18:43:31 -0800

    Calling 'source' explicitly in rvm.sh

--- packages.d/rvm.sh
+++ packages.d/rvm.sh
@@ -1,2 +1,2 @@
 # enable use of RVM (https://rvm.beginrescueend.com/)
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*
